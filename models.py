3aa7e1386fd0b894316b5a2e0b5eece737ff6fa4
Rishabh jhalani <Rishabhjhalani99@gmail.com>
2020-07-04 10:20:40 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,17 +1,11 @@
 from django.db import models
 
-class User(models.Model):
-    id = models.AutoField(primary_key=True)
+class user_profile(models.Model):
     Real_Name = models.CharField(max_length=100)
     Tz = models.CharField(max_length=100)
 
-class Activity(models.Model):
-    action_id =models.AutoField(primary_key=True)
-    Name_of_action = models.CharField(max_length=50)
+class user_activity(models.Model):
+    user_profile_id = models.ForeignKey(user_profile, on_delete=models.CASCADE)
     Action_startTime = models.DateTimeField()
     Action_endTime = models.DateTimeField()
 
-class MappTable(models.Model):
-    activity_mapping_id = models.AutoField(primary_key=True)
-    action_id = models.ForeignKey(Activity, to_field="action_id", on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, to_field="id", on_delete=models.CASCADE)
